a70dc0d600c2cb671fd0de8b1a01731f0cce3d80
ipuxin <ipuxin@qq.com>
2016-10-16 21:31:53 +0800

    Revert "搜索暂未完成"
    This reverts commit 1c7986e156d5e207f412a1b6a4c0f059a60a2ae2.

--- admin/getVerify.php
+++
@@ -1,4 +0,0 @@
-<?php
-//验证码方法的调用
-require_once '../include.php';
-verifyImage();
--- lib/common.func.php
+++
@@ -1,6 +0,0 @@
-<?php
-//提示消息公用方法
-function alertMes($mes,$url){
-	echo "<script>alert('{$mes}');</script>";
-	echo "<script>window.location='{$url}';</script>";
-}
